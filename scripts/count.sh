d7c42b5ee7ee8b434af13f56ca7d8c240a81afe3
Steven Song <steven.song98@gmail.com>
2022-06-14 15:29:55 -0500

    update script for gi-infection images

--- scripts/count.sh
+++ scripts/count.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-WORKDIR=/mnt/data5
+BACK=$PWD
 
-spaceranger count --id=colorectal-cancer-count \
+WORKDIR=/mnt/data5
+SLIDE=slide1
+AREA=A1
+SAMPLE=A1
+
+cd $WORKDIR/output/count/gi-infection/$SLIDE
+
+spaceranger count \
+  --id=$AREA \
   --transcriptome=$WORKDIR/data/ref-GRCh38-2020-A \
-  --fastqs=$WORKDIR/data/colorectal-cancer/fastqs \
-  --sample=Visium_FFPE_Human_Intestinal_Cancer \
-  --image=$WORKDIR/data/colorectal-cancer/brightfield.jpg \
-  --slide V10L13-021 \
-  --area B1 \
+  --probe-set=$WORKDIR/spaceranger/probe_sets/Visium_Human_Transcriptome_Probe_Set_v1.0_GRCh38-2020-A.csv \
+  --fastqs=$WORKDIR/data/gi-infection/$SLIDE/$AREA/fastqs \
+  --sample=AK-CW-10X-8S-$SAMPLE \
+  --image=$WORKDIR/data/gi-infection/$SLIDE/$AREA/$AREA.tif \
+  --unknown-slide \
   --localcores=16 \
-  --localmem=64
+  --localmem=48
+
+#  --slide V10L13-021 \
+#  --area B1 \
+
+cd $BACK
